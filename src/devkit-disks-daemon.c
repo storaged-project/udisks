storaged-project/udisks

commit 01f415b7e0049d71fbb221a43225d2ec7aa6404b

use object paths instead of strings for signals
Pointed out by Lennart Poettering via Jon McCann.

src/devkit-disks-daemon.c
@@ -481,7 +481,7 @@ devkit_disks_daemon_class_init (DevkitDisksDaemonClass *klass)
                               0,
                               NULL, NULL,
                               g_cclosure_marshal_VOID__STRING,
-                              G_TYPE_NONE, 1, G_TYPE_STRING);
+                              G_TYPE_NONE, 1, DBUS_TYPE_G_OBJECT_PATH);
 
         signals[DEVICE_REMOVED_SIGNAL] =
                 g_signal_new ("device-removed",
@@ -490,7 +490,7 @@ devkit_disks_daemon_class_init (DevkitDisksDaemonClass *klass)
                               0,
                               NULL, NULL,
                               g_cclosure_marshal_VOID__STRING,
-                              G_TYPE_NONE, 1, G_TYPE_STRING);
+                              G_TYPE_NONE, 1, DBUS_TYPE_G_OBJECT_PATH);
 
         signals[DEVICE_CHANGED_SIGNAL] =
                 g_signal_new ("device-changed",
@@ -499,7 +499,7 @@ devkit_disks_daemon_class_init (DevkitDisksDaemonClass *klass)
                               0,
                               NULL, NULL,
                               g_cclosure_marshal_VOID__STRING,
-                              G_TYPE_NONE, 1, G_TYPE_STRING);
+                              G_TYPE_NONE, 1, DBUS_TYPE_G_OBJECT_PATH);
 
         signals[DEVICE_JOB_CHANGED_SIGNAL] =
                 g_signal_new ("device-job-changed",
@@ -510,7 +510,7 @@ devkit_disks_daemon_class_init (DevkitDisksDaemonClass *klass)
                               devkit_disks_marshal_VOID__STRING_BOOLEAN_STRING_UINT_BOOLEAN_INT_INT_STRING_DOUBLE,
                               G_TYPE_NONE,
                               9,
-                              G_TYPE_STRING,
+                              DBUS_TYPE_G_OBJECT_PATH,
                               G_TYPE_BOOLEAN,
                               G_TYPE_STRING,
                               G_TYPE_UINT,

tools/devkit-disks.c
@@ -1075,12 +1075,12 @@ main (int argc, char **argv)
                                                  "org.freedesktop.DeviceKit.Disks",
                                                  "/",
                                                  "org.freedesktop.DeviceKit.Disks");
-        dbus_g_proxy_add_signal (disks_proxy, "DeviceAdded", G_TYPE_STRING, G_TYPE_INVALID);
-        dbus_g_proxy_add_signal (disks_proxy, "DeviceRemoved", G_TYPE_STRING, G_TYPE_INVALID);
-        dbus_g_proxy_add_signal (disks_proxy, "DeviceChanged", G_TYPE_STRING, G_TYPE_INVALID);
+        dbus_g_proxy_add_signal (disks_proxy, "DeviceAdded", DBUS_TYPE_G_OBJECT_PATH, G_TYPE_INVALID);
+        dbus_g_proxy_add_signal (disks_proxy, "DeviceRemoved", DBUS_TYPE_G_OBJECT_PATH, G_TYPE_INVALID);
+        dbus_g_proxy_add_signal (disks_proxy, "DeviceChanged", DBUS_TYPE_G_OBJECT_PATH, G_TYPE_INVALID);
         dbus_g_proxy_add_signal (disks_proxy,
                                  "DeviceJobChanged",
-                                 G_TYPE_STRING,
+                                 DBUS_TYPE_G_OBJECT_PATH,
                                  G_TYPE_BOOLEAN,
                                  G_TYPE_STRING,
                                  G_TYPE_UINT,

commit b55e244266d0969698146f40c148d0978fe0f68c

fix a bug introduced by commit 01f415b7e0049d71fbb221a43225d2ec7aa6404b
E.g. use the proper type boxed type for DBUS_G_TYPE_OBJECT_PATH for
the C closures when registering the signals

src/devkit-disks-daemon.c
@@ -480,7 +480,7 @@ devkit_disks_daemon_class_init (DevkitDisksDaemonClass *klass)
                               G_SIGNAL_RUN_LAST | G_SIGNAL_DETAILED,
                               0,
                               NULL, NULL,
-                              g_cclosure_marshal_VOID__STRING,
+                              g_cclosure_marshal_VOID__BOXED,
                               G_TYPE_NONE, 1, DBUS_TYPE_G_OBJECT_PATH);
 
         signals[DEVICE_REMOVED_SIGNAL] =
@@ -489,7 +489,7 @@ devkit_disks_daemon_class_init (DevkitDisksDaemonClass *klass)
                               G_SIGNAL_RUN_LAST | G_SIGNAL_DETAILED,
                               0,
                               NULL, NULL,
-                              g_cclosure_marshal_VOID__STRING,
+                              g_cclosure_marshal_VOID__BOXED,
                               G_TYPE_NONE, 1, DBUS_TYPE_G_OBJECT_PATH);
 
         signals[DEVICE_CHANGED_SIGNAL] =
@@ -498,7 +498,7 @@ devkit_disks_daemon_class_init (DevkitDisksDaemonClass *klass)
                               G_SIGNAL_RUN_LAST | G_SIGNAL_DETAILED,
                               0,
                               NULL, NULL,
-                              g_cclosure_marshal_VOID__STRING,
+                              g_cclosure_marshal_VOID__BOXED,
                               G_TYPE_NONE, 1, DBUS_TYPE_G_OBJECT_PATH);
 
         signals[DEVICE_JOB_CHANGED_SIGNAL] =
@@ -507,7 +507,7 @@ devkit_disks_daemon_class_init (DevkitDisksDaemonClass *klass)
                               G_SIGNAL_RUN_LAST | G_SIGNAL_DETAILED,
                               0,
                               NULL, NULL,
-                              devkit_disks_marshal_VOID__STRING_BOOLEAN_STRING_UINT_BOOLEAN_INT_INT_STRING_DOUBLE,
+                              devkit_disks_marshal_VOID__BOXED_BOOLEAN_STRING_UINT_BOOLEAN_INT_INT_STRING_DOUBLE,
                               G_TYPE_NONE,
                               9,
                               DBUS_TYPE_G_OBJECT_PATH,
@@ -762,8 +762,9 @@ device_add (DevkitDisksDaemon *daemon, DevkitDevice *d, gboolean emit_event)
                                              device);
                         g_print ("added %s\n", native_path);
                         if (emit_event) {
-                                g_signal_emit (daemon, signals[DEVICE_ADDED_SIGNAL], 0,
-                                               devkit_disks_device_local_get_object_path (device));
+                                const char *object_path;
+                                object_path = devkit_disks_device_local_get_object_path (device);
+                                g_signal_emit (daemon, signals[DEVICE_ADDED_SIGNAL], 0, object_path);
                         }
                 } else {
                         g_print ("ignoring add event on %s\n", native_path);

src/devkit-disks-marshal.list
@@ -1,2 +1,2 @@
 VOID:BOOLEAN,STRING,UINT,BOOLEAN,INT,INT,STRING,DOUBLE
-VOID:STRING,BOOLEAN,STRING,UINT,BOOLEAN,INT,INT,STRING,DOUBLE
+VOID:BOXED,BOOLEAN,STRING,UINT,BOOLEAN,INT,INT,STRING,DOUBLE