064e4ae6ee4b246a32673514f7cb7f89bc191464
David Zeuthen <davidz@redhat.com>
2010-02-23 16:48:33 -0500

    Update detach helper for current sysfs interface
    Writing 'suspend' to power/level doesn't seem to work these days
    (worked in F12 but broken in F13). Also use the new remove/ sysfs
    attribute.

--- src/helpers/job-drive-detach.c
+++ src/helpers/job-drive-detach.c
@@ -38,6 +38,62 @@ usage (void)
   g_printerr ("incorrect usage\n");
 }
 
+static gboolean
+sysfs_exists (const gchar *path,
+              const gchar *file)
+{
+  struct stat statbuf;
+  gchar *s;
+  gboolean ret;
+
+  ret = FALSE;
+
+  s = g_strdup_printf ("%s/%s", path, file);
+  if (stat (s, &statbuf) == 0)
+    ret = TRUE;
+  g_free (s);
+
+  return ret;
+}
+
+static gboolean
+sysfs_write (const gchar *path,
+             const gchar *file,
+             const gchar *value)
+{
+  FILE *f;
+  gchar *s;
+  gboolean ret;
+
+  ret = FALSE;
+  s = NULL;
+  f = NULL;
+
+  s = g_strdup_printf ("%s/%s", path, file);
+  f = fopen (s, "w");
+  if (f == NULL)
+    {
+      g_printerr ("FAILED: Cannot open %s for writing: %m\n", s);
+      goto out;
+    }
+
+  if (fwrite (value, sizeof(char), strlen (value), f) < strlen (value))
+    {
+      g_printerr ("FAILED: Error writing %s to %s: %m\n", value, s);
+      goto out;
+    }
+
+  ret = TRUE;
+
+ out:
+  g_free (s);
+
+  if (f != NULL)
+    fclose (f);
+
+  return ret;
+}
+
 int
 main (int argc,
       char *argv[])
@@ -52,9 +108,10 @@ main (int argc,
   gchar *unbind_path;
   gchar *power_level_path;
   gchar *usb_interface_name;
-  size_t usb_interface_name_len;
-  FILE *f;
   struct stat statbuf;
+  const char *bNumInterfaces;
+  gchar *endp;
+  int num_interfaces;
 
   udev = NULL;
   udevice = NULL;
@@ -86,14 +143,42 @@ main (int argc,
       goto out;
     }
 
+  udev = udev_new ();
+  if (udev == NULL)
+    {
+      g_printerr ("Error initializing libudev: %m\n");
+      goto out;
+    }
+
+  udevice = udev_device_new_from_devnum (udev, 'b', statbuf.st_rdev);
+  if (udevice == NULL)
+    {
+      g_printerr ("No udev device for device %s (devnum 0x%08x): %m\n", device, (gint) statbuf.st_rdev);
+      goto out;
+    }
+  udevice_usb_interface = udev_device_get_parent_with_subsystem_devtype (udevice, "usb", "usb_interface");
+  if (udevice_usb_interface == NULL)
+    {
+      g_printerr ("No usb parent interface for %s: %m\n", device);
+      goto out;
+    }
+  udevice_usb_device = udev_device_get_parent_with_subsystem_devtype (udevice, "usb", "usb_device");
+  if (udevice_usb_device == NULL)
+    {
+      g_printerr ("No usb parent device for %s: %m\n", device);
+      goto out;
+    }
+
+  g_printerr ("Detaching device %s\nUSB device: %s)\n",
+              device,
+              udev_device_get_syspath (udevice_usb_device));
+
   sg_fd = sg_cmds_open_device (device, 1 /* read_only */, 1);
   if (sg_fd < 0)
     {
       g_printerr ("Cannot open %s: %m\n", device);
       goto out;
     }
-
-  g_printerr ("Detaching device: %s\n", device);
 
   g_printerr ("SYNCHRONIZE CACHE: ");
   if (sg_ll_sync_cache_10 (sg_fd, 0, /* sync_nv */
@@ -137,97 +222,40 @@ main (int argc,
   sg_cmds_close_device (sg_fd);
   sg_fd = -1;
 
-  /* Now unbind the usb-storage driver from the usb interface */
-  udev = udev_new ();
-  if (udev == NULL)
-    {
-      g_printerr ("Error initializing libudev: %m\n");
-      goto out;
-    }
-
-  udevice = udev_device_new_from_devnum (udev, 'b', statbuf.st_rdev);
-  if (udevice == NULL)
-    {
-      g_printerr ("No udev device for device %s (devnum 0x%08x): %m\n", device, (gint) statbuf.st_rdev);
-      goto out;
-    }
-
   /* unbind the mass storage driver (e.g. usb-storage) */
-  udevice_usb_interface = udev_device_get_parent_with_subsystem_devtype (udevice, "usb", "usb_interface");
-  if (udevice_usb_interface == NULL)
-    {
-      g_printerr ("No usb parent interface for %s: %m\n", device);
-      goto out;
-    }
-
   usb_interface_name = g_path_get_basename (udev_device_get_devpath (udevice_usb_interface));
-  usb_interface_name_len = strlen (usb_interface_name);
-
-  g_printerr ("Unbind USB interface driver: ");
-  unbind_path = g_strdup_printf ("%s/driver/unbind", udev_device_get_syspath (udevice_usb_interface));
-  f = fopen (unbind_path, "w");
-  if (f == NULL)
-    {
-      g_printerr ("Cannot open %s for writing: %m\n", unbind_path);
-      goto out;
-    }
-  if (fwrite (usb_interface_name, sizeof(char), usb_interface_name_len, f) < usb_interface_name_len)
-    {
-      g_printerr ("Error writing %s to %s: %m\n", unbind_path, usb_interface_name);
-      fclose (f);
-      goto out;
-    }
-  fclose (f);
+  g_printerr ("Unbinding USB interface driver: ");
+  if (!sysfs_write (udev_device_get_syspath (udevice_usb_interface),
+                    "driver/unbind",
+                    usb_interface_name))
+    goto out;
   g_printerr ("OK\n");
 
-
-  /* If this is the only USB interface on the device, also suspend the
-   * USB device to e.g.  make the lights on the device power off.
-   *
-   * Failing to do so is not an error, the user may not have
-   * CONFIG_USB_SUSPEND enabled in their kernel.
-   */
-  ret = 0;
-
-  g_printerr ("Suspend USB Device: ");
-
-  udevice_usb_device = udev_device_get_parent_with_subsystem_devtype (udevice, "usb", "usb_device");
-  if (udevice_usb_device != NULL)
+  bNumInterfaces = udev_device_get_sysattr_value (udevice_usb_device, "bNumInterfaces");
+  num_interfaces = strtol (bNumInterfaces, &endp, 0);
+  if (endp != NULL && num_interfaces == 1)
     {
-      const char *bNumInterfaces;
-      char *endp;
-      int num_interfaces;
-      gchar * power_level_path;
-      gchar suspend_str[] = "suspend";
+      g_printerr ("Suspending USB device: ");
+      if (!sysfs_write (udev_device_get_syspath (udevice_usb_device), "power/level", "auto") ||
+          !sysfs_write (udev_device_get_syspath (udevice_usb_device), "power/autosuspend", "0"))
+        goto out;
+      g_printerr ("OK\n");
 
-      bNumInterfaces = udev_device_get_sysattr_value (udevice_usb_device, "bNumInterfaces");
-      num_interfaces = strtol (bNumInterfaces, &endp, 0);
-      if (endp != NULL && num_interfaces == 1)
+      /* the remove file is pretty recent (commit as1297, Dec 2009) */
+      if (sysfs_exists (udev_device_get_syspath (udevice_usb_device), "remove"))
         {
-          power_level_path = g_strdup_printf ("%s/power/level", udev_device_get_syspath (udevice_usb_device));
-          f = fopen (power_level_path, "w");
-          if (f == NULL)
-            {
-              g_printerr ("FAILED: Cannot open %s for writing: %m\n", unbind_path);
-            }
-          else
-            {
-              if (fwrite (suspend_str, sizeof(char), strlen (suspend_str), f) < strlen (suspend_str))
-                {
-                  g_printerr ("FAILED: Error writing %s to %s: %m\n", power_level_path, suspend_str);
-                }
-              else
-                {
-                  g_printerr ("OK\n");
-                }
-              fclose (f);
-            }
-        }
-      else
-        {
-          g_printerr ("Skipping since multiple USB interfaces exist.\n");
+          g_printerr ("Disabling USB port for device: ");
+          if (!sysfs_write (udev_device_get_syspath (udevice_usb_device), "remove", "1"))
+            goto out;
+          g_printerr ("OK\n");
         }
     }
+  else
+    {
+      g_printerr ("Not powering down device since multiple USB interfaces exist.\n");
+    }
+
+  ret = 0;
 
  out:
   g_free (usb_interface_name);
