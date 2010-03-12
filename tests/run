storaged-project/udisks

commit cfc669846a53815517d32d0b7b96cbc4935cea7a

Bug 26996 — wrongly detects partition on unpartitioned VFAT device
A standard VFAT header matches the signature of an MBR, so we detected
VFAT file systems on raw unpartitioned devices as both VFAT and MBR partition
table. This confuses gnome-disk-utility/gvfs, and causes them to be ignored in
GNOME.

The standard approach in the kernel/blkid is to only detect an MBR if it does
not have a valid VFAT signature. Since blkid already does that for us, just
skip the udisks-part-id invocation completely if we already know that we have a
valid ID_FS_TYPE.

data/80-udisks.rules
@@ -80,9 +80,9 @@ KERNEL=="hd*[0-9]", ATTR{removable}=="1", GOTO="probe_parttable_end"
 # don't scan for partition tables on empty discs
 KERNEL=="sr*", ENV{ID_CDROM_MEDIA}!="?*", GOTO="probe_parttable_end"
 
-# scan for partition tables both on whole-disk and partitions
-#
-IMPORT{program}="udisks-part-id $tempnode"
+# scan for partition tables both on whole-disk and partitions, unless we
+# already know that there is a file system
+ENV{ID_FS_TYPE}=="", IMPORT{program}="udisks-part-id $tempnode"
 
 LABEL="probe_parttable_end"
 

commit b2b116296d9973203c20be1a1060015ebdd1ce39

testsuite: Check partitions with filesystems
So far we only checked handling of partitions without file systems, which did
not detect the regression from commit cfc6698. Now test both combinations.

Note that we have to jump through some hoops here, since
Partition{Create,Modify} internally writes the new partition type/flags/etc,
and thus does not care about a broken part-id. Thus we trigger a change event
on block devices to actually check what the udev probers do.

tests/run
@@ -772,16 +772,29 @@ class Partitions(UDisksTestCase):
         self.assertEqual([k for k in info if k.startswith('partition_')], [])
         self.assertEqual(self.fdisk_list(), None)
 
-    def test_mbr(self):
-        '''Partitions: mbr'''
+    def test_mbr_nofs(self):
+        '''Partitions: mbr (no file system)'''
 
-        self._do_schema('mbr', '0x82', '0x06', 'boot')
+        self._do_schema('mbr', '0x82', '0x06', 'boot', filesystem=False)
 
-    def test_gpt(self):
-        '''Partitions: GUID'''
+    def test_mbr_fs(self):
+        '''Partitions: mbr (with file system)'''
+
+        self._do_schema('mbr', '0x82', '0x06', 'boot', filesystem=True)
+
+    def test_gpt_nofs(self):
+        '''Partitions: GUID (no file system)'''
 
         self._do_schema('gpt', 'EBD0A0A2-B9E5-4433-87C0-68B6B72699C7', 
-                '0657FD6D-A4AB-43C4-84E5-0933C84B4F4F', 'required')
+                '0657FD6D-A4AB-43C4-84E5-0933C84B4F4F', 'required',
+                filesystem=False)
+
+    def test_gpt_fs(self):
+        '''Partitions: GUID (with file system)'''
+
+        self._do_schema('gpt', 'EBD0A0A2-B9E5-4433-87C0-68B6B72699C7', 
+                '0657FD6D-A4AB-43C4-84E5-0933C84B4F4F', 'required',
+                filesystem=True)
 
     # TODO: fails in various ways
     def disabled_test_apm(self):
@@ -799,7 +812,7 @@ class Partitions(UDisksTestCase):
                 first_offset=3000000)
 
     def _do_schema(self, schema, type1, type2, flag,
-            exp_default_partitions=0, first_offset=0):
+            exp_default_partitions=0, first_offset=0, filesystem=False):
         '''Run tests for a particular schema/type'''
 
         # create partition table
@@ -821,11 +834,21 @@ class Partitions(UDisksTestCase):
         self.assertEqual(props.Get(I_D, 'PartitionTableScheme'), schema)
         self.assertEqual(props.Get(I_D, 'PartitionTableCount'), exp_default_partitions)
 
-        # p1: non-flagged, no fs
-        p1 = self.partition_iface().PartitionCreate(first_offset, 10000000, type1, 
-                '', [], [], '', [])
+        # p1: non-flagged
+        if filesystem:
+            p1 = self.partition_iface().PartitionCreate(first_offset, 10000000,
+                    type1, '', [], [], 'vfat', ['label=testp1'])
+        else:
+            p1 = self.partition_iface().PartitionCreate(first_offset, 10000000,
+                    type1, '', [], [], '', [])
         self.assert_(p1 in self.manager_iface.EnumerateDevices())
  
+        # udisks internally sets the new partition flags from the
+        # PartitionCreate() call; trigger udev to ensure that we actually read
+        # and check the values from the udev probers
+        subprocess.call(['udevadm', 'trigger', '--action=change', '--subsystem-match=block'])
+        self.sync()
+
         if schema == 'mbr':
             fdisk = self.fdisk_list()
             self.assertEqual(len(fdisk), 1)
@@ -862,8 +885,14 @@ class Partitions(UDisksTestCase):
         self.assertEqual(p1_props.Get(I_D, 'PartitionLabel'), (schema == 'apm' and 'untitled' or ''))
         self.assertEqual(p1_props.Get(I_D, 'PartitionFlags'), [])
         self.assertEqual(p1_props.Get(I_D, 'PartitionNumber'), (schema == 'apm' and 2 or 1))
-        self.assertEqual(p1_props.Get(I_D, 'IdUsage'), '')
-        self.assertEqual(p1_props.Get(I_D, 'IdType'), '')
+        if filesystem:
+            self.assertEqual(p1_props.Get(I_D, 'IdUsage'), 'filesystem')
+            self.assertEqual(p1_props.Get(I_D, 'IdType'), 'vfat')
+            self.assertEqual(p1_props.Get(I_D, 'IdLabel'), 'testp1')
+        else:
+            self.assertEqual(p1_props.Get(I_D, 'IdUsage'), '')
+            self.assertEqual(p1_props.Get(I_D, 'IdType'), '')
+            self.assertEqual(p1_props.Get(I_D, 'IdLabel'), '')
         off = p1_props.Get(I_D, 'PartitionOffset')
         self.assert_(off >= first_offset and off <= first_offset+20000)
         size = p1_props.Get(I_D, 'PartitionSize')
@@ -872,6 +901,10 @@ class Partitions(UDisksTestCase):
 
         # modify
         p1_iface.PartitionModify(type2, '', [flag])
+        # udisks internally sets the new partition flags from the
+        # PartitionCreate() call; trigger udev to ensure that we actually read
+        # and check the values from the udev probers
+        subprocess.call(['udevadm', 'trigger', '--action=change', '--subsystem-match=block'])
         self.sync()
         self.assertEqual(p1_props.Get(I_D, 'PartitionType'), type2)
         self.assertEqual(p1_props.Get(I_D, 'PartitionFlags'), [flag])