559194f2c7a07d97ef06bedbb17373efa11387fe
David Zeuthen <davidz@redhat.com>
2010-09-02 17:54:47 -0400

    Rename ProbedBlockDevice to BlockDevice.Probed
    Signed-off-by: David Zeuthen <davidz@redhat.com>

--- src/linuxblock.c
+++ src/linuxblock.c
@@ -44,7 +44,7 @@ typedef struct
   UDisksBlockDevice *iface_block_device;
 
   /* interfaces that may or may not be implemented */
-  UDisksProbedBlockDevice *iface_probed_block_device;
+  UDisksBlockDeviceProbed *iface_block_device_probed;
 } LinuxBlock;
 
 static void
@@ -58,8 +58,8 @@ linux_block_free (LinuxBlock *block)
     g_object_unref (block->iface_linux_sysfs_device);
   if (block->iface_block_device != NULL)
     g_object_unref (block->iface_block_device);
-  if (block->iface_probed_block_device != NULL)
-    g_object_unref (block->iface_probed_block_device);
+  if (block->iface_block_device_probed != NULL)
+    g_object_unref (block->iface_block_device_probed);
   g_free (block);
 }
 
@@ -69,7 +69,7 @@ static void
 linux_block_update (LinuxBlock  *block,
                     const gchar *uevent_action)
 {
-  gboolean add_probed_block_device;
+  gboolean add_block_device_probed;
 
   /* org.freedesktop.UDisks.LinuxSysfsDevice */
   if (block->iface_linux_sysfs_device == NULL)
@@ -104,39 +104,39 @@ linux_block_update (LinuxBlock  *block,
       g_dbus_object_add_interface (block->object, G_DBUS_INTERFACE (block->iface_block_device));
     }
 
-  /* org.freedesktop.UDisks.ProbedBlockDevice */
-  add_probed_block_device = FALSE;
-  if (block->iface_probed_block_device == NULL)
+  /* org.freedesktop.UDisks.BlockDevice.Probed */
+  add_block_device_probed = FALSE;
+  if (block->iface_block_device_probed == NULL)
     {
       if (g_udev_device_has_property (block->device, "ID_FS_USAGE"))
         {
-          block->iface_probed_block_device = udisks_probed_block_device_stub_new ();
-          add_probed_block_device = TRUE;
+          block->iface_block_device_probed = udisks_block_device_probed_stub_new ();
+          add_block_device_probed = TRUE;
         }
     }
   else
     {
       if (!g_udev_device_has_property (block->device, "ID_FS_USAGE"))
         {
-          g_dbus_object_remove_interface (block->object, G_DBUS_INTERFACE (block->iface_probed_block_device));
-          g_object_unref (block->iface_probed_block_device);
-          block->iface_probed_block_device = NULL;
+          g_dbus_object_remove_interface (block->object, G_DBUS_INTERFACE (block->iface_block_device_probed));
+          g_object_unref (block->iface_block_device_probed);
+          block->iface_block_device_probed = NULL;
         }
     }
-  if (block->iface_probed_block_device != NULL)
+  if (block->iface_block_device_probed != NULL)
     {
-      udisks_probed_block_device_set_usage (block->iface_probed_block_device,
+      udisks_block_device_probed_set_usage (block->iface_block_device_probed,
                                             g_udev_device_get_property (block->device, "ID_FS_USAGE"));
-      udisks_probed_block_device_set_kind (block->iface_probed_block_device,
+      udisks_block_device_probed_set_kind (block->iface_block_device_probed,
                                            g_udev_device_get_property (block->device, "ID_FS_TYPE"));
-      udisks_probed_block_device_set_version (block->iface_probed_block_device,
+      udisks_block_device_probed_set_version (block->iface_block_device_probed,
                                               g_udev_device_get_property (block->device, "ID_FS_VERSION"));
-      udisks_probed_block_device_set_label (block->iface_probed_block_device,
+      udisks_block_device_probed_set_label (block->iface_block_device_probed,
                                             g_udev_device_get_property (block->device, "ID_FS_LABEL_ENC"));
-      udisks_probed_block_device_set_uuid (block->iface_probed_block_device,
+      udisks_block_device_probed_set_uuid (block->iface_block_device_probed,
                                            g_udev_device_get_property (block->device, "ID_FS_UUID_ENC"));
-      if (add_probed_block_device)
-        g_dbus_object_add_interface (block->object, G_DBUS_INTERFACE (block->iface_probed_block_device));
+      if (add_block_device_probed)
+        g_dbus_object_add_interface (block->object, G_DBUS_INTERFACE (block->iface_block_device_probed));
     }
 }
 
