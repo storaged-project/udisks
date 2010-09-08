17e506f014c62ca9f3a86d4c26897d6b112be3c6
David Zeuthen <davidz@redhat.com>
2010-09-08 17:51:07 -0400

    Build fixes / cleanups
    Signed-off-by: David Zeuthen <davidz@redhat.com>

---
+++ udisks/udiskstypes.h
@@ -0,0 +1,35 @@
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*-
+ *
+ * Copyright (C) 2007-2010 David Zeuthen <zeuthen@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#if !defined (__UDISKS_INSIDE_UDISKS_H__) && !defined (UDISKS_COMPILATION)
+#error "Only <udisks/udisks.h> can be included directly."
+#endif
+
+#ifndef __UDISKS_TYPES_H__
+#define __UDISKS_TYPES_H__
+
+#include <gio/gio.h>
+#include <udisks/udisksenums.h>
+
+G_BEGIN_DECLS
+
+G_END_DECLS
+
+#endif /* __UDISKS_TYPES_H__ */
