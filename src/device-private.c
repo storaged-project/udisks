858adbac9dce63da52764b2d2157bc7782b304a5
Ayan George <ayan.george@canonical.com>
2011-06-23 13:27:06 -0400

    Bug 38535 – A DeviceAutoMountHint attribute should be added to udisks
    With minor changes by David Zeuthen, see the bug for details.
    
    https://bugs.freedesktop.org/show_bug.cgi?id=38535
    
    Signed-off-by: David Zeuthen <davidz@redhat.com>

--- src/device-private.c
+++ src/device-private.c
@@ -109,6 +109,18 @@ ptr_str_array_from_strv (GStrv s)
 }
 
 void
+device_set_device_auto_mount_hint (Device *device,
+                                   const gchar *value)
+{
+  if (G_UNLIKELY (g_strcmp0 (device->priv->device_auto_mount_hint, value) != 0))
+    {
+      g_free (device->priv->device_auto_mount_hint);
+      device->priv->device_auto_mount_hint = g_strdup (value);
+      emit_changed (device, "device_auto_mount_hint");
+    }
+}
+
+void
 device_set_device_detection_time (Device *device,
                                   guint64 value)
 {
