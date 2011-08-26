b47cb3ae8c1cbc89867d41c787112dd07656a47b
David Zeuthen <davidz@redhat.com>
2011-08-26 13:09:40 -0400

    Revert "throw_error(): Print error to stderr when being called internally"
    This reverts commit af80cd56e3b68cbc0de1fa1d03f9b2b2463aa514.

--- src/device.c
+++ src/device.c
@@ -5281,15 +5281,12 @@ throw_error (DBusGMethodInvocation *context,
   va_list args;
   char *message;
 
+  if (context == NULL)
+    return TRUE;
+
   va_start (args, format);
   message = g_strdup_vprintf (format, args);
   va_end (args);
-
-  if (context == NULL)
-    {
-      g_warning ("%s", message);
-      return TRUE;
-    }
 
   error = g_error_new (ERROR, error_code, "%s", message);
   dbus_g_method_return_error (context, error);
