1e6ee09cefc7ebf8d46ab8016952858e179f888d
David Zeuthen <davidz@redhat.com>
2012-05-01 00:05:21 -0400

    Loop: add :Autoclear property and SetAutoclear() method
    Also add some UI in Disks for this
    
     http://people.freedesktop.org/~david/disks-loop-autoclear-switch.png
    
    Signed-off-by: David Zeuthen <davidz@redhat.com>

--- src/udiskslinuxloop.c
+++ src/udiskslinuxloop.c
@@ -27,6 +27,14 @@
 #include <string.h>
 #include <stdlib.h>
 
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+
+#include <linux/loop.h>
+
 #include <glib/gstdio.h>
 
 #include "udiskslogging.h"
@@ -148,6 +156,8 @@ udisks_linux_loop_update (UDisksLinuxLoop        *loop,
     {
       udisks_loop_set_backing_file (UDISKS_LOOP (loop), "");
     }
+  udisks_loop_set_autoclear (UDISKS_LOOP (loop),
+                             g_udev_device_get_sysfs_attr_as_boolean (device, "loop/autoclear"));
   g_object_unref (device);
 }
 
@@ -255,11 +265,158 @@ handle_delete (UDisksLoop             *loop,
   return TRUE; /* returning TRUE means that we handled the method invocation */
 }
 
+/* ---------------------------------------------------------------------------------------------------- */
+
+static gboolean
+loop_set_autoclear (const gchar  *device,
+                    gboolean      value,
+                    GError      **error)
+{
+  gboolean ret = FALSE;
+  struct loop_info64 li64;
+  gint fd = -1;
+
+  g_return_val_if_fail (error == NULL || *error == NULL, FALSE);
+
+  fd = open (device, O_RDWR);
+  if (fd == -1)
+    {
+      g_set_error (error,
+                   G_IO_ERROR,
+                   g_io_error_from_errno (errno),
+                   "Error opening loop device %s: %m",
+                   device);
+      goto out;
+    }
+
+  memset (&li64, '\0', sizeof (li64));
+  if (ioctl (fd, LOOP_GET_STATUS64, &li64) < 0)
+    {
+      g_set_error (error,
+                   G_IO_ERROR,
+                   g_io_error_from_errno (errno),
+                   "Error getting status for loop device %s: %m",
+                   device);
+      goto out;
+    }
+
+  if (value)
+    li64.lo_flags |= LO_FLAGS_AUTOCLEAR;
+  else
+    li64.lo_flags &= (~LO_FLAGS_AUTOCLEAR);
+
+  if (ioctl (fd, LOOP_SET_STATUS64, &li64) < 0)
+    {
+      g_set_error (error,
+                   G_IO_ERROR,
+                   g_io_error_from_errno (errno),
+                   "Error setting status for loop device %s: %m",
+                   device);
+      goto out;
+    }
+
+  ret = TRUE;
+
+ out:
+  if (fd != -1 )
+    {
+      if (close (fd) != 0)
+        udisks_warning ("close(2) on loop fd %d for device %s failed: %m", fd, device);
+    }
+  return ret;
+}
+
+/* runs in thread dedicated to handling @invocation */
+static gboolean
+handle_set_autoclear (UDisksLoop             *loop,
+                      GDBusMethodInvocation  *invocation,
+                      gboolean                arg_value,
+                      GVariant               *options)
+{
+  UDisksObject *object = NULL;
+  UDisksBlock *block = NULL;
+  UDisksDaemon *daemon = NULL;
+  UDisksCleanup *cleanup = NULL;
+  GError *error = NULL;
+  uid_t caller_uid = -1;
+  uid_t setup_by_uid = -1;
+
+  error = NULL;
+  object = udisks_daemon_util_dup_object (loop, &error);
+  if (object == NULL)
+    {
+      g_dbus_method_invocation_take_error (invocation, error);
+      goto out;
+    }
+
+  block = udisks_object_peek_block (object);
+  daemon = udisks_linux_block_object_get_daemon (UDISKS_LINUX_BLOCK_OBJECT (object));
+  cleanup = udisks_daemon_get_cleanup (daemon);
+
+  error = NULL;
+  if (!udisks_daemon_util_get_caller_uid_sync (daemon, invocation, NULL, &caller_uid, NULL, NULL, &error))
+    {
+      g_dbus_method_invocation_return_gerror (invocation, error);
+      g_error_free (error);
+      goto out;
+    }
+
+  if (!udisks_cleanup_has_loop (cleanup,
+                                udisks_block_get_device (block),
+                                &setup_by_uid))
+    {
+      setup_by_uid = -1;
+    }
+
+  if (caller_uid != setup_by_uid)
+    {
+      if (!udisks_daemon_util_check_authorization_sync (daemon,
+                                                        object,
+                                                        "org.freedesktop.udisks2.loop-modify-others",
+                                                        options,
+                                                        /* Translators: Shown in authentication dialog when the user
+                                                         * requests changing autoclear on a loop device set up by
+                                                         * another user.
+                                                         *
+                                                         * Do not translate $(udisks2.device), it's a placeholder and
+                                                         * will be replaced by the name of the drive/device in question
+                                                         */
+                                                        N_("Authentication is required to modify the loop device $(udisks2.device)"),
+                                                        invocation))
+        goto out;
+    }
+
+  error = NULL;
+  if (!loop_set_autoclear (udisks_block_get_device (block),
+                           arg_value,
+                           &error))
+    {
+      g_dbus_method_invocation_take_error (invocation, error);
+      goto out;
+    }
+
+  /* specutatively update our local value so a change signal is emitted before we return... */
+  udisks_loop_set_autoclear (UDISKS_LOOP (loop), arg_value);
+  g_dbus_interface_skeleton_flush (G_DBUS_INTERFACE_SKELETON (loop));
+
+  /* ... but make sure we update the property value from sysfs */
+  udisks_linux_block_object_trigger_uevent (UDISKS_LINUX_BLOCK_OBJECT (object));
+
+  /* TODO: would be better to have something like trigger_uevent_and_wait_for_it_sync() */
+
+  udisks_loop_complete_set_autoclear (loop, invocation);
+
+ out:
+  g_clear_object (&object);
+
+  return TRUE; /* returning TRUE means that we handled the method invocation */
+}
 
 /* ---------------------------------------------------------------------------------------------------- */
 
 static void
 loop_iface_init (UDisksLoopIface *iface)
 {
-  iface->handle_delete = handle_delete;
+  iface->handle_delete        = handle_delete;
+  iface->handle_set_autoclear = handle_set_autoclear;
 }
