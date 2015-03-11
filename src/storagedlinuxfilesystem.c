bfabb4e22c4ba50ca8dd3c5af3f46b9f0ba7a1a3
Martin Pitt <martin.pitt@ubuntu.com>
2015-03-11 13:52:40 +0100

    Drop default [df]mask for VFAT and NTFS
    The parent mount point dir (/run/media/user) already controls access
    permissions, so there is no need to set [df]mask to 077 for VFAT and
    NTFS. This makes UDISKS_FILESYSTEM_SHARED="1" actually work for these
    file systems.
    
    https://launchpad.net/bugs/453605

--- src/storagedlinuxfilesystem.c
+++ src/storagedlinuxfilesystem.c
@@ -282,7 +282,7 @@ typedef struct
 
 /* ---------------------- vfat -------------------- */
 
-static const gchar *vfat_defaults[] = { "uid=", "gid=", "shortname=mixed", "dmask=0077", "utf8=1", "showexec", "flush", NULL };
+static const gchar *vfat_defaults[] = { "uid=", "gid=", "shortname=mixed", "utf8=1", "showexec", "flush", NULL };
 static const gchar *vfat_allow[] = { "flush", "utf8=", "shortname=", "umask=", "dmask=", "fmask=", "codepage=", "iocharset=", "usefree", "showexec", NULL };
 static const gchar *vfat_allow_uid_self[] = { "uid=", NULL };
 static const gchar *vfat_allow_gid_self[] = { "gid=", NULL };
@@ -290,7 +290,7 @@ static const gchar *vfat_allow_gid_self[] = { "gid=", NULL };
 /* ---------------------- ntfs -------------------- */
 /* this is assuming that ntfs-3g is used */
 
-static const gchar *ntfs_defaults[] = { "uid=", "gid=", "dmask=0077", "fmask=0177", NULL };
+static const gchar *ntfs_defaults[] = { "uid=", "gid=", NULL };
 static const gchar *ntfs_allow[] = { "umask=", "dmask=", "fmask=", "locale=", "norecover", "ignore_case", "windows_names", "compression", "nocompression", NULL };
 static const gchar *ntfs_allow_uid_self[] = { "uid=", NULL };
 static const gchar *ntfs_allow_gid_self[] = { "gid=", NULL };
