85d1bd38c6eb476b2ec0f245d103fe014f524bda
Thomas Gläßle <t_glaessle@gmx.de>
2015-07-06 16:17:29 +0200

    Use binary IO channel for input_string
    The channel encoding defines to which encoding the input string should
    be transformed before writing it to the channel. Since the in-memory
    encoding is always UTF-8 and the default encoding is also UTF-8, this is
    a NO-OP by default — except that it checks that all characters are valid
    UTF-8 characters. To avoid this check the encoding must be set to NULL
    (=binary). Note that the actual transformation (=NO-OP) remains the
    same.

--- src/udisksspawnedjob.c
+++ src/udisksspawnedjob.c
@@ -979,6 +979,8 @@ void udisks_spawned_job_start (UDisksSpawnedJob *job)
       job->input_string_cursor = job->input_string;
 
       job->child_stdin_channel = g_io_channel_unix_new (job->child_stdin_fd);
+      // we want to write binary, suppress checking the encoding:
+      g_io_channel_set_encoding (job->child_stdin_channel, NULL, NULL);
       g_io_channel_set_flags (job->child_stdin_channel, G_IO_FLAG_NONBLOCK, NULL);
       job->child_stdin_source = g_io_create_watch (job->child_stdin_channel, G_IO_OUT);
       g_source_set_callback (job->child_stdin_source, (GSourceFunc) write_child_stdin, job, NULL);
