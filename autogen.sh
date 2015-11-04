5a9f0defc79ff234b1ee54d6f1ef16308331d9d9
Peter Hatina <phatina@redhat.com>
2015-11-04 09:03:51 +0100

    Build: remove deprecated variable in autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -4,8 +4,6 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="storaged"
-
 (test -f $srcdir/src/Makefile.am) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
