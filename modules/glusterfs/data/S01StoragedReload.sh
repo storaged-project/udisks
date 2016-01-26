5481e930e8a858d76b47346ec8405d3ec873d519
Samikshan Bairagya <samikshan@gmail.com>
2016-01-26 19:43:53 +0530

    Remove script from modules/glusterfs/data
    The script should be a part of the glusterfs code repository and
    not in storaged. Through this script storaged is able to know of
    changes (only volume life-cycle changes like volume creation,
    deletion, starting, stopping, etc.) in gluster state

--- modules/glusterfs/data/S01StoragedReload.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-dbus-send --system                               \
---dest=org.storaged.Storaged --type=method_call  \
-/org/storaged/Storaged/Manager                   \
-org.storaged.Storaged.Manager.GlusterFS.Reload
