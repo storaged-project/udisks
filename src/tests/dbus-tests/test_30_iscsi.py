0948e329b766730eb4ae765bd26c59ff71bc56fe
Tomas Smetana <tomas@smetana.name>
2016-06-30 12:50:16 +0200

    Tests: Add basic D-Bus tests for LVM and iSCSI
    These are just the basic LVM and iSCSI tests borrowing lot of code and ideas
    from the integration_test already present in the tree. The tests are by no
    means complete and would need to be extended. They intentionally don't rely on
    the gi python module.
    
    The tests are meant to be run on a dedicated machine: they connect to the system
    bus and may disrupt the running system.

---
+++ src/tests/dbus-tests/test_30_iscsi.py
@@ -0,0 +1,30 @@
+import unittest
+import storagedtestcase
+import dbus
+import os
+import glob
+import time
+
+class StoragedISCSITest(storagedtestcase.StoragedTestCase):
+    '''Basic iSCSI test suite'''
+
+    def test_login_noauth(self):
+        host = os.getenv('TEST_ISCSI_HOST')
+        self.assertIsNotNone(host)
+        manager = self.get_object('', '/Manager')
+        (nodes, nodes_num) = manager.DiscoverSendTargets(host, 0, self.no_options,
+                dbus_interface=self.iface_prefix + '.Manager.ISCSI.Initiator')
+        self.assertGreater(nodes_num, 0)
+        (iqn, tpg, host, port, iface) = nodes[0]
+        manager.Login(iqn, tpg, host, port, iface, self.no_options,
+                dbus_interface=self.iface_prefix + '.Manager.ISCSI.Initiator')
+        time.sleep(1)
+        devs = glob.glob('/dev/disk/by-path/*%s*' % iqn)
+        self.assertGreater(len(devs), 0)
+        manager.Logout(iqn, tpg, host, port, iface, self.no_options,
+                dbus_interface=self.iface_prefix + '.Manager.ISCSI.Initiator')
+        time.sleep(1)
+        devs = glob.glob('/dev/disk/by-path/*%s*' % iqn)
+        self.assertEqual(len(devs), 0)
+
+
