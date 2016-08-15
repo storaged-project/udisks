1cbefcebe4c6bedf2b7bc3a43a445ab73f7e5f9c
Marius Vollmer <mvollmer@redhat.com>
2016-08-15 13:57:01 +0300

    mdraid: Fix job expected end time

--- src/udiskslinuxmdraid.c
+++ src/udiskslinuxmdraid.c
@@ -286,7 +286,6 @@ udisks_linux_mdraid_update (UDisksLinuxMDRaid       *mdraid,
   guint64 chunk_size = 0;
   gdouble sync_completed_val = 0.0;
   guint64 sync_rate = 0;
-  guint64 sync_start_time = 0;
   guint64 sync_remaining_time = 0;
   GVariantBuilder builder;
   UDisksDaemon *daemon = NULL;
@@ -447,9 +446,8 @@ udisks_linux_mdraid_update (UDisksLinuxMDRaid       *mdraid,
           udisks_job_set_progress_valid (UDISKS_JOB (job), TRUE);
           udisks_job_set_rate (UDISKS_JOB (job), sync_rate);
 
-          sync_start_time = udisks_job_get_start_time (UDISKS_JOB (job));
           udisks_job_set_expected_end_time (UDISKS_JOB (job),
-                                            sync_start_time + sync_remaining_time);
+                                            g_get_real_time () + sync_remaining_time);
         }
       else
         {
