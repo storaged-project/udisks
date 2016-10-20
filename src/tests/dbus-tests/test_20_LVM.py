5e7a58452da96b5ad732d042bb1f8d9531f58862
Vojtech Trefny <vtrefny@redhat.com>
2016-10-20 13:02:30 +0200

    Few fixes for lvm plugin dbus tests
    Use addCleanup to delete create VGs, pep8/pylint fixes etc.

--- src/tests/dbus-tests/test_20_LVM.py
+++ src/tests/dbus-tests/test_20_LVM.py
@@ -1,32 +1,32 @@
-import unittest
-import storagedtestcase
 import dbus
 import os
-import subprocess
 import time
+
+import storagedtestcase
+
 
 class StoragedLVMTest(storagedtestcase.StoragedTestCase):
     '''This is a basic LVM test suite'''
 
     def _create_vg(self, vgname, devices):
-        self.udev_settle() # Since the devices might not be ready yet
+        self.udev_settle()  # Since the devices might not be ready yet
         manager = self.get_object('', '/Manager')
         vg_path = manager.VolumeGroupCreate(vgname, devices, self.no_options,
-                dbus_interface=self.iface_prefix + '.Manager.LVM2')
+                                            dbus_interface=self.iface_prefix + '.Manager.LVM2')
         self.udev_settle()
         time.sleep(0.5)
         vg = self.bus.get_object(self.iface_prefix, vg_path)
         self.assertIsNotNone(vg)
-        self.assertEqual(subprocess.call(['vgs', vgname]), 0)
+        ret, _out = self.run_command('vgs %s' % vgname)
+        self.assertEqual(ret, 0)
         return vg
-
 
     def _remove_vg(self, vg):
         vgname = self.get_property(vg, '.VolumeGroup', 'Name')
         vg.Delete(True, self.no_options, dbus_interface=self.iface_prefix + '.VolumeGroup')
         self.udev_settle()
-        self.assertNotEqual(subprocess.call(['vgs', vgname]), 0)
-
+        ret, _out = self.run_command('vgs %s' % vgname)
+        self.assertNotEqual(ret, 0)
 
     def test_10_linear(self):
         '''Test linear (plain) LV functionality'''
@@ -40,6 +40,7 @@ class StoragedLVMTest(storagedtestcase.StoragedTestCase):
             self.assertIsNotNone(dev_obj)
             devs.append(dev_obj)
         vg = self._create_vg(vgname, devs)
+        self.addCleanup(self._remove_vg, vg)
 
         # Create linear LV on the VG
         vgsize = int(self.get_property(vg, '.VolumeGroup', 'Size'))
@@ -47,10 +48,13 @@ class StoragedLVMTest(storagedtestcase.StoragedTestCase):
         self.assertEqual(vgsize, vg_freesize)
         lvname = 'storaged_test_lv'
         lv_path = vg.CreatePlainVolume(lvname, dbus.UInt64(vgsize), self.no_options,
-                dbus_interface=self.iface_prefix + '.VolumeGroup')
+                                       dbus_interface=self.iface_prefix + '.VolumeGroup')
         self.udev_settle()
         self.assertIsNotNone(lv_path)
-        self.assertEqual(subprocess.call(['lvs', os.path.join(vgname, lvname)]), 0)
+
+        ret, _out = self.run_command('lvs %s' % os.path.join(vgname, lvname))
+        self.assertEqual(ret, 0)
+
         lv = self.bus.get_object(self.iface_prefix, lv_path)
         lv_block_path = lv.Activate(self.no_options, dbus_interface=self.iface_prefix + '.LogicalVolume')
         self.assertIsNotNone(lv_block_path)
@@ -106,11 +110,9 @@ class StoragedLVMTest(storagedtestcase.StoragedTestCase):
         lv.Deactivate(self.no_options, dbus_interface=self.iface_prefix + '.LogicalVolume')
         lv.Delete(self.no_options, dbus_interface=self.iface_prefix + '.LogicalVolume')
         self.udev_settle()
-        self.assertNotEqual(subprocess.call(['lvs', os.path.join(vgname, lvname)]), 0)
 
-        # vgremove
-        self._remove_vg(vg)
-
+        ret, _out = self.run_command('lvs %s' % os.path.join(vgname, lvname))
+        self.assertNotEqual(ret, 0)
 
     def test_20_thin(self):
         '''Test thin volumes functionality'''
@@ -124,26 +126,32 @@ class StoragedLVMTest(storagedtestcase.StoragedTestCase):
             self.assertIsNotNone(dev_obj)
             devs.append(dev_obj)
         vg = self._create_vg(vgname, devs)
+        self.addCleanup(self._remove_vg, vg)
 
         # Create thin pool on the VG
         vgsize = int(self.get_property(vg, '.VolumeGroup', 'FreeSize'))
         tpname = 'storaged_test_tp'
         tp_path = vg.CreateThinPoolVolume(tpname, dbus.UInt64(vgsize), self.no_options,
-                dbus_interface=self.iface_prefix + '.VolumeGroup')
+                                          dbus_interface=self.iface_prefix + '.VolumeGroup')
         self.udev_settle()
         self.assertIsNotNone(tp_path)
-        self.assertEqual(subprocess.call(['lvs', os.path.join(vgname, tpname)]), 0)
+
+        ret, _out = self.run_command('lvs %s' % os.path.join(vgname, tpname))
+        self.assertEqual(ret, 0)
+
         tp = self.bus.get_object(self.iface_prefix, tp_path)
         tpsize = int(self.get_property(tp, '.LogicalVolume', 'Size'))
 
         # Create thin volume in the pool with virtual size twice the backing pool
         tvname = 'storaged_test_tv'
         tv_path = vg.CreateThinVolume(tvname, dbus.UInt64(tpsize * 2), tp, self.no_options,
-                dbus_interface=self.iface_prefix + '.VolumeGroup')
+                                      dbus_interface=self.iface_prefix + '.VolumeGroup')
         self.udev_settle()
         tv = self.bus.get_object(self.iface_prefix, tv_path)
         self.assertIsNotNone(tv)
-        self.assertEqual(subprocess.call(['lvs', os.path.join(vgname, tvname)]), 0)
+
+        ret, _out = self.run_command('lvs %s' % os.path.join(vgname, tvname))
+        self.assertEqual(ret, 0)
 
         # Check the block device of the thin volume
         lv_block_path = tv.Activate(self.no_options, dbus_interface=self.iface_prefix + '.LogicalVolume')
@@ -152,10 +160,6 @@ class StoragedLVMTest(storagedtestcase.StoragedTestCase):
         self.assertIsNotNone(lv_block)
         blocksize = int(self.get_property(lv_block, '.Block', 'Size'))
         self.assertGreater(blocksize, vgsize)
-
-        # vgremove
-        self._remove_vg(vg)
-
 
     def test_30_snapshot(self):
         '''Test LVM snapshoting'''
@@ -169,30 +173,30 @@ class StoragedLVMTest(storagedtestcase.StoragedTestCase):
             self.assertIsNotNone(dev_obj)
             devs.append(dev_obj)
         vg = self._create_vg(vgname, devs)
+        self.addCleanup(self._remove_vg, vg)
 
         # Create the origin LV
         vgsize = int(self.get_property(vg, '.VolumeGroup', 'FreeSize'))
         lvname = 'storaged_test_origin_lv'
         lv_path = vg.CreatePlainVolume(lvname, dbus.UInt64(vgsize / 2), self.no_options,
-                dbus_interface=self.iface_prefix + '.VolumeGroup')
+                                       dbus_interface=self.iface_prefix + '.VolumeGroup')
         self.udev_settle()
         lv = self.bus.get_object(self.iface_prefix, lv_path)
         self.assertIsNotNone(lv)
-        self.assertEqual(subprocess.call(['lvs', os.path.join(vgname, lvname)]), 0)
+
+        ret, _out = self.run_command('lvs %s' % os.path.join(vgname, lvname))
+        self.assertEqual(ret, 0)
+
         time.sleep(1)
 
         # Create the LV's snapshot
         snapname = 'storaged_test_snap_lv'
         vg_freesize = int(self.get_property(vg, '.VolumeGroup', 'FreeSize'))
         snap_path = lv.CreateSnapshot(snapname, vg_freesize, self.no_options,
-                dbus_interface=self.iface_prefix + '.LogicalVolume')
+                                      dbus_interface=self.iface_prefix + '.LogicalVolume')
         self.udev_settle()
         snap = self.bus.get_object(self.iface_prefix, snap_path)
         self.assertIsNotNone(snap)
-
-        # vgremove
-        self._remove_vg(vg)
-
 
     def test_40_cache(self):
         '''Basic LVM cache test'''
@@ -206,6 +210,7 @@ class StoragedLVMTest(storagedtestcase.StoragedTestCase):
             self.assertIsNotNone(dev_obj)
             devs.append(dev_obj)
         vg = self._create_vg(vgname, devs)
+        self.addCleanup(self._remove_vg, vg)
 
         # Create the origin LV
         vgsize = int(self.get_property(vg, '.VolumeGroup', 'FreeSize'))
@@ -215,7 +220,9 @@ class StoragedLVMTest(storagedtestcase.StoragedTestCase):
         self.udev_settle()
         lv = self.bus.get_object(self.iface_prefix, lv_path)
         self.assertIsNotNone(lv)
-        self.assertEqual(subprocess.call(['lvs', os.path.join(vgname, lvname)]), 0)
+
+        ret, _out = self.run_command('lvs %s' % os.path.join(vgname, orig_lvname))
+        self.assertEqual(ret, 0)
         time.sleep(1)
 
         # Create the caching LV
