7b3338920d52e4f9deb9ad29f35b4a38a59324c0
Vojtech Trefny <vtrefny@redhat.com>
2016-11-30 15:52:31 +0100

    Add dbus tests for job objects
    'safe_dbus' module allows us to use dbus from multiple threads.
    This part is copied from Anaconda installer codebase.

---
+++ src/tests/dbus-tests/test_job.py
@@ -0,0 +1,129 @@
+import datetime
+import os
+import time
+import threading
+
+import gi
+gi.require_version('GLib', '2.0')
+from gi.repository import GLib
+
+import safe_dbus
+import storagedtestcase
+
+
+class StoragedJobTest(storagedtestcase.StoragedTestCase):
+    '''This is a basic test suite for job objects and interface'''
+
+    def setUp(self):
+        self.job = None
+        self.exception = None
+
+    def _get_objects(self):
+        objects = safe_dbus.call_sync(self.iface_prefix,
+                                      self.path_prefix,
+                                      'org.freedesktop.DBus.ObjectManager',
+                                      'GetManagedObjects',
+                                      None)
+        return objects
+
+    def _secure_erase(self, devname):
+        try:
+            safe_dbus.call_sync(self.iface_prefix,
+                                self.path_prefix + '/block_devices/' + devname,
+                                self.iface_prefix + '.Block',
+                                'Format',
+                                GLib.Variant('(sa{sv})', ('empty', {'erase': GLib.Variant("s", 'zero')})))
+        except Exception as e:
+            self.exception = e
+
+    def _wait_for_job_thread(self, operation, device_path):
+        t = threading.currentThread()
+
+        while getattr(t, "run", True):
+            objects = self._get_objects()
+
+            jobs = {k: v for (k, v) in objects[0].items() if '/jobs/' in k}
+
+            for job_path, properties in jobs.items():
+                if properties[self.iface_prefix + '.Job']['Operation'] == operation and \
+                   properties[self.iface_prefix + '.Job']['Objects'] == [device_path]:
+                    self.job = (job_path, properties)
+                    return
+
+            time.sleep(0.1)
+
+    def test_job(self):
+        '''Test basic Job functionality and properties'''
+
+        disk_name = os.path.basename(self.vdevs[0])
+        obj_path = self.path_prefix + '/block_devices/' + disk_name
+
+        start_time = datetime.datetime.now().timestamp()
+
+        watch_thread = threading.Thread(target=self._wait_for_job_thread, args=('format-erase', obj_path))
+        watch_thread.start()
+
+        erase_thread = threading.Thread(target=self._secure_erase, args=(disk_name,))
+        erase_thread.start()
+        erase_thread.join()
+
+        # unexpected exception occured in erase thread -- raise it
+        if self.exception is not None:
+            raise self.exception
+
+        # erase thread finished, stop job searching
+        watch_thread.run = False
+        watch_thread.join()
+
+        # we should have the job dict now
+        self.assertIsNotNone(self.job)
+
+        # get all the properties from the dict
+        properties = self.job[1][self.iface_prefix + '.Job']
+
+        _ret, disk_size = self.run_command('lsblk -b -no SIZE %s' % self.vdevs[0])  # get size of the device
+        self.assertEqual(properties['Bytes'], int(disk_size))
+
+        self.assertEqual(properties['StartedByUID'], os.getuid())
+
+        # test start time -- should be less than ~0.5s after thread started
+        # (dbus property is in micro seconds)
+        self.assertLessEqual(abs(properties['StartTime'] - start_time * 10**6), 0.5 * 10**6)
+
+        self.assertTrue(properties['Cancelable'])
+
+    def test_cancel(self):
+        '''Test if it's possible to cancel the Job'''
+
+        disk_name = os.path.basename(self.vdevs[0])
+        obj_path = self.path_prefix + '/block_devices/' + disk_name
+
+        watch_thread = threading.Thread(target=self._wait_for_job_thread, args=('format-erase', obj_path))
+        watch_thread.start()
+
+        erase_thread = threading.Thread(target=self._secure_erase, args=(disk_name,))
+        erase_thread.start()
+
+        # watch thread should end first -- we need the job before erase finishes
+        watch_thread.join()
+        self.assertIsNotNone(self.job)
+
+        job_path = self.job[0]
+
+        # cancel the job
+        safe_dbus.call_sync(self.iface_prefix,
+                            job_path,
+                            self.iface_prefix + '.Job',
+                            'Cancel',
+                            GLib.Variant('(a{sv})', ({},)))
+
+        # job shouldn't be in managed objects
+        objects = self._get_objects()
+        self.assertNotIn(job_path, objects[0].items())
+
+        erase_thread.join()
+
+        # erase thread should raise exception (it's stored in self.exception)
+        self.assertIsNotNone(self.exception)
+        self.assertTrue(isinstance(self.exception, safe_dbus.DBusCallError))
+        self.assertIn('Error erasing device: Job was canceled', str(self.exception))
