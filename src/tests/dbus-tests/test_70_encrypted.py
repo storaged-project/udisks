b662d664b6298f038fd6219acf279a5fb915571f
Marius Vollmer <mvollmer@redhat.com>
2019-05-04 14:44:27 +0300

    manager: Expose supported encryption types and their default
    For the benefit of UIs such as Cockpit.

--- src/tests/dbus-tests/test_70_encrypted.py
+++ src/tests/dbus-tests/test_70_encrypted.py
@@ -456,17 +456,9 @@ class UdisksEncryptedTestLUKS2(UdisksEncryptedTest):
         self.assertEqual(clear_size3, clear_size)
 
     def _get_default_luks_version(self):
-        if not os.path.exists(UDISKS_CONFIG_FILE):
-            self.fail('UDisks config file not found.')
-
-        config = configparser.ConfigParser()
-        config.read(UDISKS_CONFIG_FILE)
-
-        if 'defaults' not in config:
-            self.fail('Failed to read defaults from UDisks config file.')
-
-        # get luks version from 'luks1' or 'luks2'
-        return config['defaults']['encryption'][-1]
+        manager = self.get_object('/Manager')
+        default_encryption_type = self.get_property(manager, '.Manager', 'DefaultEncryptionType')
+        return default_encryption_type.value[-1]
 
     def test_create_default(self):
         disk_name = os.path.basename(self.vdevs[0])
@@ -487,5 +479,25 @@ class UdisksEncryptedTestLUKS2(UdisksEncryptedTest):
         dbus_version = self.get_property(disk, '.Block', 'IdVersion')
         dbus_version.assertEqual(default_version)
 
+    def test_suppported_encryption_types(self):
+        manager = self.get_object('/Manager')
+        supported_encryption_types = self.get_property(manager, '.Manager', 'SupportedEncryptionTypes')
+        supported_encryption_types.assertLen(2)
+        supported_encryption_types.assertContains("luks1")
+        supported_encryption_types.assertContains("luks2")
+
+    def test_default_encryption_type(self):
+        if not os.path.exists(UDISKS_CONFIG_FILE):
+            self.fail('UDisks config file not found.')
+
+        config = configparser.ConfigParser()
+        config.read(UDISKS_CONFIG_FILE)
+
+        if 'defaults' not in config:
+            self.fail('Failed to read defaults from UDisks config file.')
+
+        manager = self.get_object('/Manager')
+        default_encryption_type = self.get_property(manager, '.Manager', 'DefaultEncryptionType')
+        default_encryption_type.assertEqual(config['defaults']['encryption'])
 
 del UdisksEncryptedTest  # skip UdisksEncryptedTest
