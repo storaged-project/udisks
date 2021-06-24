1ea5d068bb3108aa1b1c4a2f31f9537c19568549
Tomas Bzatek <tbzatek@redhat.com>
2021-06-24 13:59:41 +0200

    udiskslinuxblock: Do not force wipe when exclusive lock on a block device cannot be acquired
    This will cause bd_fs_wipe() to fail when the block device is in use (e.g. mounted).

--- src/udiskslinuxblock.c
+++ src/udiskslinuxblock.c
@@ -3100,7 +3100,11 @@ udisks_linux_block_handle_format (UDisksBlock             *block,
   device_name = udisks_block_dup_device (block);
 
   /* First wipe the device... */
-  if (! bd_fs_wipe (device_name, TRUE, &error))
+#ifdef HAVE_LIBBLOCKDEV3
+  if (! bd_fs_wipe (device_name, TRUE, FALSE, &error))
+#else
+  if (! bd_fs_wipe_force (device_name, TRUE, FALSE, &error))
+#endif
     {
       if (g_error_matches (error, BD_FS_ERROR, BD_FS_ERROR_NOFS))
         /* no signature to remove, ignore */
