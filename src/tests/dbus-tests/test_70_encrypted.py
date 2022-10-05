38839fb2ee8d502a0991065178043ffdfe0227e8
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-05 13:47:30 +0000

    Adding tarfile member sanitization to extractall()

--- src/tests/dbus-tests/test_70_encrypted.py
+++ src/tests/dbus-tests/test_70_encrypted.py
@@ -685,7 +685,26 @@ class UdisksEncryptedTestBITLK(udiskstestcase.UdisksTestCase):
         self.tempdir = tempfile.mkdtemp(prefix='udisks_test_bitlk')
         images = os.path.join(os.path.dirname(__file__), 'bitlk-images.tar.gz')
         with tarfile.open(images, 'r') as tar:
-            tar.extractall(self.tempdir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, self.tempdir)
 
         with open(os.path.join(self.tempdir, self.bitlk_img), 'r+b') as loop_file:
             fd = loop_file.fileno()
